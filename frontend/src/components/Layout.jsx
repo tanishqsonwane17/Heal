b5c3318caa9816e491998539d124dd31a32a4cfc
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-08-19 18:06:27 +0530

    gender page completed

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router'
-
+import { IoIosArrowBack } from "react-icons/io";
 const Layout = () => {
   return (
     <>
+    <div className='h-20 w-full flex items-center gap-1.5 px-4'>
+      <div className='h-10 flex justify-center items-center w-10 rounded-full border'>
+         <IoIosArrowBack className='text-2xl '/>
+      </div>
+      <h6>Assessment</h6>
+    </div>
     <Outlet/>
     </>
   )
--- frontend/src/components/Step2Gender.jsx
+++ frontend/src/components/Step2Gender.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { useFormContext } from "react-hook-form";
 import { UserContext } from "../context/UserContext";
-
+import { IoIosArrowRoundForward } from "react-icons/io";
 const Step2Gender = () => {
   const { register, watch, setValue } = useFormContext();
   const { nextStep, prevStep } = useContext(UserContext);
@@ -11,14 +11,14 @@ const Step2Gender = () => {
   const options = ["Male", "Female", "Other"];
 
   return (
-    <div className="w-full px-4 mt-10 flex flex-col items-center justify-between  ">
+    <div className="w-full bg-[linear-gradient(to_right,_##fde1e198_10%,_white_99%)] px-4 mt-2 flex flex-col items-center justify-between  ">
       <div>
       <div className=" w-full flex justify-center ">
         <h1 className="text-[28px] leading-tight text-center font-medium text-[#352414]">
           What's your official gender
         </h1>
       </div>
-      <div className="flex flex-col gap-70 h-full">
+      <div className="flex flex-col gap-60 h-full">
       <div className=" flex flex-col gap-4 mt-10  items-center w-full">
         {options.map((gender) => (
           <div
@@ -39,13 +39,14 @@ const Step2Gender = () => {
       
     </div>
       <div className=" w-full  ">
-        <button className="bg-[#4F3422] mb-10 text-white py-3 w-full rounded-3xl tracking-wider font-semibold"
-          type="button"
-          onClick={nextStep}
-          disabled={!selectedGender}
-          >
-          Continue
-        </button>
+        <button
+        className={`mb-10 text-white py-3 w-full rounded-3xl tracking-wider font-semibold ${selectedGender ? "bg-[#4F3422]" : "bg-gray-400 cursor-not-allowed"}`}
+        type="button"
+        onClick={nextStep}
+        disabled={!selectedGender}
+      >
+        Continue <IoIosArrowRoundForward className="text-4xl inline" />
+      </button>
       </div>
       </div>
       </div>
